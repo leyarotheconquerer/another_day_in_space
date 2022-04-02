87f207496badf72b05ec0d6798c3a22fbdea2695
leyarotheconquerer <leyarotheconquerer@yahoo.com>
2022-04-02 13:11:58 -0500

    Add speed attribute

---
+++ Source/AnotherDay/Ships/APlayerShip.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "APlayerShip.generated.h"
+
+UCLASS()
+class ANOTHERDAY_API AAPlayerShip : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Ship")
+	int Speed;
+	
+	// Sets default values for this character's properties
+	AAPlayerShip();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
