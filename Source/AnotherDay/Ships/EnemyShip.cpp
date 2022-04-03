a1e538bba06980397d772a92d9a6b661b091b072
leyarotheconquerer <leyarotheconquerer@yahoo.com>
2022-04-02 19:16:01 -0500

    Add enemy ship and station actors

---
+++ Source/AnotherDay/Ships/EnemyShip.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyShip.h"
+
+
+// Sets default values
+AEnemyShip::AEnemyShip()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AEnemyShip::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AEnemyShip::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AEnemyShip::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
