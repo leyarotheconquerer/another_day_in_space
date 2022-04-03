61c6d4fc5c1ccb6bb895a0723f5815e121acf858
leyarotheconquerer <leyarotheconquerer@yahoo.com>
2022-04-03 14:06:12 -0500

    Add health to player

---
+++ Source/AnotherDay/Health.cpp
@@ -0,0 +1,70 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Health.h"
+
+
+// Sets default values for this component's properties
+UHealth::UHealth()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UHealth::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UHealth::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+
+void UHealth::TakeDamage(float damage, const UDamageType* type)
+{
+	const TSubclassOf<UDamageType> typeClass = type->GetClass();
+	if (DamageTypes.Contains(typeClass))
+	{
+		EDamageType* damageType = DamageTypes.Find(typeClass);
+		float shieldFactor = 1.f;
+		float armorFactor = 1.f;
+		if (damageType)
+		{
+			switch (*damageType)
+			{
+			case EDamageType::BEAM:
+				shieldFactor = 2.f;
+				armorFactor = 0.5f;
+				break;
+			case EDamageType::MISSILE:
+				shieldFactor = 0.5f;
+				armorFactor = 2.f;
+				break;
+			case EDamageType::NORMAL:
+			default:
+				break;
+			}
+		}
+		const float remaining = FMath::Max(0.f, (damage * shieldFactor - Shield) / shieldFactor);
+		Shield = FMath::Max(0.f, Shield - damage * shieldFactor);
+		Armor = FMath::Max(0.f, Armor - remaining * armorFactor);
+		if (Armor <= 0.001f)
+		{
+			Die.Broadcast();
+		}
+	}
+}
---
+++ Source/AnotherDay/Health.h
@@ -0,0 +1,63 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Health.generated.h"
+
+UENUM(BlueprintType)
+enum class EDamageType : uint8
+{
+	NORMAL = 0 UMETA(DisplayName = "Normal"),
+	BEAM = 1 UMETA(DisplayName = "Beam"),
+	MISSILE = 2 UMETA(DisplayName = "Missile")
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FHealthEvent);
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class ANOTHERDAY_API UHealth : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Armor")
+	float Armor;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Armor")
+	float ArmorTotal;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Shield")
+	float Shield;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Shield")
+	float ShieldTotal;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Damage")
+	TMap<TSubclassOf<UDamageType>, EDamageType> DamageTypes;
+
+	UPROPERTY(BlueprintAssignable, Category="Health")
+	FHealthEvent Die;
+	
+	// Sets default values for this component's properties
+	UHealth();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(
+		float DeltaTime, ELevelTick TickType,
+		FActorComponentTickFunction* ThisTickFunction
+	) override;
+
+	UFUNCTION(BlueprintCallable, Category="Health")
+	void TakeDamage(float damage, const UDamageType* type);
+
+	// UFUNCTION(BlueprintImplementableEvent, Category="Health")
+	// void Die();
+};
