b36cda47b05140782a909116c69938767a81e524
leyarotheconquerer <leyarotheconquerer@yahoo.com>
2022-04-02 19:08:02 -0500

    Remove unnecessary struct

--- Source/AnotherDay/Ships/APlayerShip.h
+++ Source/AnotherDay/Ships/APlayerShip.h
@@ -3,25 +3,8 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "AnotherDay/Weapons/Weapon.h"
-#include "AnotherDay/Weapons/Projectile.h"
 #include "GameFramework/Character.h"
 #include "APlayerShip.generated.h"
-
-USTRUCT(BlueprintType)
-struct FShipWeapon
-{
-	GENERATED_BODY()
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Weapon")
-	float RotationOffset;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Weapon")
-	TArray<FName> Turrets;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Weapon")
-	TSubclassOf<AProjectile> Type;
-};
 
 UCLASS()
 class ANOTHERDAY_API AAPlayerShip : public ACharacter
@@ -29,18 +12,6 @@ class ANOTHERDAY_API AAPlayerShip : public ACharacter
 	GENERATED_BODY()
 
 public:
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Ship")
-	int Speed;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Weapon")
-	FShipWeapon Weapon1;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Weapon")
-	FShipWeapon Weapon2;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Weapon")
-	FShipWeapon Weapon3;
-
 	// Sets default values for this character's properties
 	AAPlayerShip();
 
